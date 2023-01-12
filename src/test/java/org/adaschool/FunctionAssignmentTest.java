eced6869d0b981b8faa433d53969e95168faaf8c
sancarbar <sancabar@gmail.com>
2023-01-11 23:23:13 -0500

    Initial commit.

---
+++ src/test/java/org/adaschool/FunctionAssignmentTest.java
@@ -0,0 +1,11 @@
+package org.adaschool;
+
+import org.junit.jupiter.api.Test;
+
+public class FunctionAssignmentTest {
+
+    @Test
+    void isPersonUnderAgeFunctionAssignmentTest() {
+//        Assertions.assertEquals(true, false);
+    }
+}
