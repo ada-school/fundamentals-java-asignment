b29cc75059339c9e91e007bef2abcd01e38185a9
sancarbar <sancabar@gmail.com>
2023-01-12 22:26:52 -0500

    Update test with Assertion.

--- src/main/java/org/adaschool/Main.java
+++ src/main/java/org/adaschool/Main.java
@@ -3,5 +3,10 @@ package org.adaschool;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        functionToImplement();
+    }
+
+    public static boolean functionToImplement() {
+        return true;
     }
 }
--- src/test/java/org/adaschool/FunctionAssignmentTest.java
+++ src/test/java/org/adaschool/FunctionAssignmentTest.java
@@ -1,11 +1,12 @@
 package org.adaschool;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FunctionAssignmentTest {
 
     @Test
     void isPersonUnderAgeFunctionAssignmentTest() {
-//        Assertions.assertEquals(true, false);
+        Assertions.assertEquals(Main.functionToImplement(), true);
     }
 }
