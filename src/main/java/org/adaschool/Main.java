c9095688bd6c0546341b1af0f5112cb56c4aea0c
sancarbar <sancabar@gmail.com>
2023-01-12 23:16:45 -0500

    Implemented Unit test for function implementation exercise.

--- src/main/java/org/adaschool/Main.java
+++ src/main/java/org/adaschool/Main.java
@@ -2,11 +2,12 @@ package org.adaschool;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        functionToImplement();
+        System.out.println("Running Test");
+        isValidEmailAddress("ada@ada-school.org");
     }
 
-    public static boolean functionToImplement() {
-        return true;
+    public static boolean isValidEmailAddress(String emailAddress) {
+        //TODO implement this method and validate that email address is correct, returning true or false accordingly.
+        return false;
     }
 }
--- src/test/java/org/adaschool/FunctionAssignmentTest.java
+++ src/test/java/org/adaschool/FunctionAssignmentTest.java
@@ -6,7 +6,46 @@ import org.junit.jupiter.api.Test;
 public class FunctionAssignmentTest {
 
     @Test
-    void isPersonUnderAgeFunctionAssignmentTest() {
-        Assertions.assertEquals(Main.functionToImplement(), true);
+    void isValidEmailTrueTest() {
+        try {
+            Assertions.assertTrue(Main.isValidEmailAddress("ada@ada-school.org"));
+        } catch (AssertionError error) {
+            String errorMessage = "Your validation does not accept the valid email address: ada@ada-school.org";
+            System.out.println(errorMessage);
+            throw new AssertionError(errorMessage, error);
+        }
+    }
+
+    @Test
+    void isNotValidEmailTest1() {
+        try {
+            Assertions.assertFalse(Main.isValidEmailAddress("adaada-school.org"));
+        } catch (AssertionError error) {
+            String errorMessage = "Your validation does not cover @ mandatory character validation";
+            System.out.println(errorMessage);
+            throw new AssertionError(errorMessage, error);
+        }
+    }
+
+    @Test
+    void isNotValidEmailTest2() {
+        try {
+            Assertions.assertFalse(Main.isValidEmailAddress("adaada-schoolorg"));
+        } catch (AssertionError error) {
+            String errorMessage = "Your validation does not cover . email extension validation(.com, .org, .co, etc)";
+            System.out.println(errorMessage);
+            throw new AssertionError(errorMessage, error);
+        }
+    }
+
+    @Test
+    void isNotValidEmailEmptyTest() {
+        try {
+            Assertions.assertFalse(Main.isValidEmailAddress(""));
+        } catch (AssertionError error) {
+            String errorMessage = "Your validation does not cover empty strings";
+            System.out.println(errorMessage);
+            throw new AssertionError(errorMessage, error);
+        }
     }
 }
